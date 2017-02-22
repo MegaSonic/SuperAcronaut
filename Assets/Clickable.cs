ecf8d5e27fa28d3f63ec6d5eb94c30aa765b79bf
Adam Kay <u0988616@ad.utah.edu>
2017-02-22 15:30:21 -0700

    Testing mechanics here

---
+++ Assets/Clickable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour {
+
+    private NoituLoveCharacter player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<NoituLoveCharacter>(); ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        player.dashTarget = transform;
+        player.isDashing = true;
+    }
+}
---
+++ Assets/NoituLoveCharacter.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prime31;
+
+
+public class NoituLoveCharacter : MonoBehaviour {
+
+    public enum State
+    {
+        IDLE = 0,
+        DASHING = 1,
+        ATTACKING = 2,
+        RUNNING = 3
+    }
+
+    public NoituLoveCharacter.State charState;
+
+
+    public float runSpeed = 7;
+    public float dashSpeed = 12;
+
+    public float gravity = -40;
+    public float terminalVelocity = -12;
+
+    public Vector3 velocity;
+    public bool _disableGravity;
+    public bool readInput;
+    public bool isDashing;
+
+    public float distanceToDashTo;
+
+    public Transform dashTarget;
+
+    private CharacterController2D _controller;
+    private BoxCollider2D _collider;
+
+
+
+    private void Awake()
+    {
+        readInput = true;
+        _controller = GetComponent<CharacterController2D>();
+        _collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        velocity = _controller.velocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (readInput)
+        {
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                velocity.x = runSpeed * -1;
+            }
+            else if (Input.GetAxis("Horizontal") > 0)
+            {
+                velocity.x = runSpeed;
+            }
+            else
+            {
+                velocity.x = 0f;
+            }
+        }
+
+        if (isDashing)
+        {
+            readInput = false;
+            _disableGravity = true;
+            Vector3 movementDirection = dashTarget.position - this.transform.position;
+            movementDirection = movementDirection.normalized;
+            movementDirection *= dashSpeed;
+            velocity = movementDirection;
+            velocity.z = 0f;
+
+            if ((dashTarget.position - transform.position).magnitude < distanceToDashTo)
+            {
+                readInput = true;
+                isDashing = false;
+                _disableGravity = false;
+            }
+        }
+
+        if (!_disableGravity)
+        {
+            velocity.y += gravity * Time.deltaTime;
+            if (velocity.y < terminalVelocity)
+            {
+                velocity.y = terminalVelocity;
+            }
+        }
+
+        velocity *= Time.deltaTime;
+        _controller.move(velocity);
+        velocity = _controller.velocity;
+    }
+}
